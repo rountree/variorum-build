18baeb8d361fae28395bb2bfa8e87a156bf7ee2c
Barry Rountree <rountree@llnl.gov>
2020-09-05 04:44:36 +0000

    Force either Debug or release build.

--- myconfigure.sh
+++ myconfigure.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-
-cmake -DCMAKE_INSTALL_PREFIX=${HOME}/install/variorum -C ${HOME}/repos/variorum/host-configs/packet-ubuntu.20.04.LTS-x86_64-gcc@9.3.0 ${HOME}/repos/variorum/src
+cmake 											\
+	-DCMAKE_BUILD_TYPE="Release"							\
+	-DCMAKE_INSTALL_PREFIX=${HOME}/install/variorum 				\
+	-C ${HOME}/build/variorum-build/packet-ubuntu.20.04.LTS-x86_64-gcc@9.3.0 	\
+	${HOME}/repos/variorum/src
 
